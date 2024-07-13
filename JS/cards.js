107cd2442695ca16838fdcc6d4c748658d5bb8c7
Dominik Mijalski <domindomin514@gmail.com>
2024-07-13 17:11:46 +0200

    to delete

--- JS/cards.js
+++ JS/cards.js
@@ -1,13 +1,38 @@
-let videoDiv = document.querySelectorAll('.video__element');
-let videoDescription = document.querySelectorAll('.video__additional__content');
-let seeMore = document.querySelectorAll('.click__to__see');
+let videoDiv = document.querySelectorAll(".video__element");
+let videoDescription = document.querySelectorAll(".video__additional__content");
+let seeMore = document.querySelectorAll(".click__to__see");
 
 // const showDescription = (element) => {
 // 	console.log(element);
 // 	element.classlist.add('showDescription');
 seeMore.forEach((item) => {
-	item.addEventListener('click', () => {
-		item.parentElement.previousElementSibling.style.opacity = 0;
-		item.parentElement.classList.add('showDescription');
+	item.addEventListener("click", () => {
+		if (
+			item.parentElement.previousElementSibling.classList.contains("rotate")
+		) {
+			item.parentElement.previousElementSibling.classList.remove("rotate");
+			item.previousElementSibling.classList.remove("rotatesecondback");
+			item.parentElement.previousElementSibling.classList.add("rotate");
+			item.previousElementSibling.classList.add("unrotatecard");
+} 
+
+
+
+
+
+else if (
+
+			item.parentElement.previousElementSibling.classList.contains("rotateback")
+		) {
+			item.parentElement.previousElementSibling.classList.remove("rotateback");
+			item.previousElementSibling.classList.remove("rotatesecondback");
+			item.parentElement.previousElementSibling.classList.add("rotate");
+			item.previousElementSibling.classList.add("unrotatecard");
+		} else {
+			item.parentElement.previousElementSibling.classList.add("rotate");
+			item.previousElementSibling.classList.add("unrotatecard");
+		}
+		// item.previousElementSibling.classList.add("rotatesecondback");
+		// item.previousElementSibling.classList.remove("unrotatecard");
 	});
 });
