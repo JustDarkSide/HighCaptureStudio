212b725dd98da31a18712ddc0d0865dba26f3acd
Kacper <krzysztofzajac975@gmail.com>
2024-07-13 19:25:05 +0200

    now mobile view for tablet devices are need to be done

--- JS/cards.js
+++ JS/cards.js
@@ -1,38 +1,30 @@
-let videoDiv = document.querySelectorAll(".video__element");
-let videoDescription = document.querySelectorAll(".video__additional__content");
-let seeMore = document.querySelectorAll(".click__to__see");
+let videoDiv = document.querySelectorAll('.video__element');
+let videoDescription = document.querySelectorAll('.video__additional__content');
+let seeMore = document.querySelectorAll('.click__to__see');
 
-// const showDescription = (element) => {
-// 	console.log(element);
-// 	element.classlist.add('showDescription');
 seeMore.forEach((item) => {
-	item.addEventListener("click", () => {
+	item.addEventListener('click', () => {
 		if (
-			item.parentElement.previousElementSibling.classList.contains("rotate")
+			item.previousElementSibling.firstElementChild.classList.contains(
+				'showDescription'
+			)
 		) {
-			item.parentElement.previousElementSibling.classList.remove("rotate");
-			item.previousElementSibling.classList.remove("rotatesecondback");
-			item.parentElement.previousElementSibling.classList.add("rotate");
-			item.previousElementSibling.classList.add("unrotatecard");
-} 
-
-
-
-
-
-else if (
-
-			item.parentElement.previousElementSibling.classList.contains("rotateback")
-		) {
-			item.parentElement.previousElementSibling.classList.remove("rotateback");
-			item.previousElementSibling.classList.remove("rotatesecondback");
-			item.parentElement.previousElementSibling.classList.add("rotate");
-			item.previousElementSibling.classList.add("unrotatecard");
+			item.previousElementSibling.firstElementChild.classList.remove(
+				'showDescription'
+			);
+			item.previousElementSibling.firstElementChild.classList.add(
+				'hideDescription'
+			);
+			item.firstElementChild.textContent = 'O projekcie';
 		} else {
-			item.parentElement.previousElementSibling.classList.add("rotate");
-			item.previousElementSibling.classList.add("unrotatecard");
+			item.previousElementSibling.firstElementChild.classList.remove(
+				'hideDescription'
+			);
+			item.previousElementSibling.firstElementChild.classList.add(
+				'showDescription'
+			);
+			item.firstElementChild.textContent = 'Zobacz video';
 		}
-		// item.previousElementSibling.classList.add("rotatesecondback");
-		// item.previousElementSibling.classList.remove("unrotatecard");
+		item.firstElementChild.classList.toggle('rotate');
 	});
 });
