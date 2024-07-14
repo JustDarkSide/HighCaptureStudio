9c3fde08aa486d6e1c53bd8cc733144b61ee331f
Kacper <krzysztofzajac975@gmail.com>
2024-07-14 10:15:36 +0200

    changes after push but before pull

--- JS/cards.js
+++ JS/cards.js
@@ -4,27 +4,27 @@ let seeMore = document.querySelectorAll('.click__to__see');
 
 seeMore.forEach((item) => {
 	item.addEventListener('click', () => {
-		if (
-			item.previousElementSibling.firstElementChild.classList.contains(
-				'showDescription'
-			)
-		) {
-			item.previousElementSibling.firstElementChild.classList.remove(
-				'showDescription'
-			);
-			item.previousElementSibling.firstElementChild.classList.add(
-				'hideDescription'
-			);
-			item.firstElementChild.textContent = 'O projekcie';
-		} else {
-			item.previousElementSibling.firstElementChild.classList.remove(
-				'hideDescription'
-			);
-			item.previousElementSibling.firstElementChild.classList.add(
-				'showDescription'
-			);
-			item.firstElementChild.textContent = 'Zobacz video';
-		}
-		item.firstElementChild.classList.toggle('rotate');
+		// if (
+		// 	item.previousElementSibling.firstElementChild.classList.contains(
+		// 		'showDescription'
+		// 	)
+		// ) {
+		// 	item.previousElementSibling.firstElementChild.classList.remove(
+		// 		'showDescription'
+		// 	);
+		// 	item.previousElementSibling.firstElementChild.classList.add(
+		// 		'hideDescription'
+		// 	);
+		// 	item.firstElementChild.textContent = 'O projekcie';
+		// } else {
+		// 	item.previousElementSibling.firstElementChild.classList.remove(
+		// 		'hideDescription'
+		// 	);
+		// 	item.previousElementSibling.firstElementChild.classList.add(
+		// 		'showDescription'
+		// 	);
+		// 	item.firstElementChild.textContent = 'Zobacz video';
+		// }
+		item.firstElementChild.setAttribute('src', '');
 	});
 });
