8369bd26ba84756b301cc2a342f04038576f9346
Dominik Mijalski <domindomin514@gmail.com>
2024-08-04 15:27:20 +0200

    changed a bit

--- JS/nav.js
+++ JS/nav.js
@@ -1,39 +1,39 @@
-let burgerIcon = document.querySelector('.hamburger');
-let navExternalPart = document.querySelector('.nav__external__part');
-let burgerIconBars = document.querySelector('.hamburger-inner');
-let navElements = document.querySelectorAll('.nav__external__part__item');
+let burgerIcon = document.querySelector(".hamburger");
+let navExternalPart = document.querySelector(".nav__external__part");
+let burgerIconBars = document.querySelector(".hamburger-inner");
+let navElements = document.querySelectorAll(".nav__external__part__item");
 
 const manageNav = () => {
-	burgerIcon.classList.toggle('is-active');
-	navExternalPart.classList.toggle('activated');
+	burgerIcon.classList.toggle("is-active");
+	navExternalPart.classList.toggle("activated");
 	if (
-		window.getComputedStyle(burgerIcon).getPropertyValue('position') ==
-		'absolute'
+		window.getComputedStyle(burgerIcon).getPropertyValue("position") ==
+		"absolute"
 	) {
-		burgerIcon.style.position = 'fixed';
-		document.body.style.overflow = 'hidden';
-		if (window.innerWidth >= 1200) {
-			burgerIcon.style.left = '88%';
+		burgerIcon.style.position = "fixed";
+		document.body.style.overflow = "hidden";
+		if (window.innerWidth >= 768) {
+			burgerIcon.style.left = "1.5%";
 		} else {
-			burgerIcon.style.left = '80%';
+			burgerIcon.style.left = "80%";
 		}
 	} else {
-		burgerIcon.style.position = 'absolute';
-		document.body.style.overflow = 'auto';
-		document.body.style.overflowX = 'hidden';
+		burgerIcon.style.position = "absolute";
+		document.body.style.overflow = "auto";
+		document.body.style.overflowX = "hidden";
 
-		if (window.innerWidth >= 1200) {
-			burgerIcon.style.left = '60%';
+		if (window.innerWidth >= 768) {
+			burgerIcon.style.left = "5%";
 		} else {
-			burgerIcon.style.left = '50%';
+			burgerIcon.style.left = "50%";
 		}
 	}
 };
 
 navElements.forEach((element) => {
-	element.addEventListener('click', manageNav);
+	element.addEventListener("click", manageNav);
 });
 
-burgerIcon.addEventListener('click', () => {
+burgerIcon.addEventListener("click", () => {
 	manageNav();
 });
